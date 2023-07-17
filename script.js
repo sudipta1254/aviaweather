b8c269fc4e8ab6a28d882e41dce3683361470b86
Sudipta Singha <115441575+sudipta1254@users.noreply.github.com>
2023-07-17 13:00:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function user() {
    fetch(url)
    .then(response => response.json())
    .then(data => {
-      console.log(data[0]);
+      //console.log(data[0]);
       main(data[0]);
    })
    .catch(err => {
