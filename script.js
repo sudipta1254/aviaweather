d2d2d86a718c12e27934db0f52d02e89219cb93f
Sudipta Singha <115441575+sudipta1254@users.noreply.github.com>
2023-09-11 22:07:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -459,7 +459,7 @@ function cwxTafMain(data, flag, comp) {
     b.eq(26).text(type.toUpperCase());
     if (data.remarks)
         b.eq(27).text(data.remarks);
-    b.eq(27).html(data.station.name+', '+data.station.location+` <img src="https://flagcdn.com/24x18/${flag}.png">`);
+    b.eq(28).html(data.station.name+', '+data.station.location+` <img src="https://flagcdn.com/24x18/${flag}.png">`);
     b.eq(29).text(data.icao);
     b.eq(30).text(getIST(data.timestamp.issued)+' '+time(data.timestamp.issued));
     b.eq(31).text(getIST(data.timestamp.from)+' until '+getIST(data.timestamp.to));
